8d290941947d616977859587c523ca7bd08ec112
Daniil Khoroshev <daniil.khoroshev@netcracker.com>
2024-02-11 17:05:30 +0400

    inital commit

---
+++ src/main/java/com/example/esa_lab_2/EsaLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.esa_lab_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaLab2Application.class, args);
+	}
+
+}
